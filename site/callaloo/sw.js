10ff2798e2aeefe99ad23a461f2f5cdf726d1893
Prisca Dezigns <priscadezigns9@gmail.com>
2026-05-23 15:16:10 -0400

    calalloo: Add depth and PWA components for site/callaloo/sw.js

--- site/callaloo/sw.js
+++ site/callaloo/sw.js
@@ -1,29 +1,12 @@
-// Callaloo Service Worker v3
-const CACHE = "callaloo-v3";
+const CACHE_NAME = 'calalloo-v1';
 const ASSETS = [
-  "/callaloo/",
-  "/callaloo/index.html",
-  "/callaloo/recipes/",
-  "/callaloo/scan/",
-  "/callaloo/feed/",
-  "/callaloo/blog/",
-  "/callaloo/waitlist/",
+  '/site/callaloo/login/',
+  '/site/callaloo/dashboard/',
+  '/site/callaloo/index.html'
 ];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", e => {
-  if(e.request.method !== "GET") return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
