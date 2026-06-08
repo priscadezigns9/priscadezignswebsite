5a4dca76c5f58d266aba4987f682805143aa4846
Priscion <priscadezigns9@gmail.com>
2026-06-08 14:46:40 -0400

    PWA: Update Nurasen Service Worker Logic

--- nurasen/sw.js
+++ nurasen/sw.js
@@ -1,4 +1,21 @@
-const CACHE_NAME = 'nurasen-v2';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'nurasen-v1';
+const urlsToCache = [
+  '/nurasen/',
+  '/nurasen/index.html',
+  '/nurasen/dashboard.html',
+  '/logos/NURASEN_DEFINITIVE_2026.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
