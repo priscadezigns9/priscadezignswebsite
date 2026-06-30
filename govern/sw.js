c8e6c649ec9ee3a97b704b8a0ef957c30ceef62f
PD <priscadezigns9@gmail.com>
2026-06-30 08:05:28 -0400

    Delete govern directory

--- govern/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'trogon-v1';
-const ASSETS = ['/govern/dashboard/', '/govern/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
